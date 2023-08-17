5df4f27c0d221fcf52b1f04bf52b39d8fd7fb6b7
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-08-17 22:14:38 +0500

    q

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import './globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/js/bootstrap.bundle'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
